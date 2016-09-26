8c6a620d795db128b0e3bf269da49e887c7810e5
Naoki <naoki826divide@gmail.com>
2016-09-26 08:38:53 +0000

    first

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
+     collection do
+      post :confirm
+    end
+  end
+
+  resources :contacts, only: [:new, :create] do
+    collection do
+      post :confirm
+    end
+  end
+
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    omniauth_callbacks: "users/omniauth_callbacks"
+}
+
+  root 'blog#index'
+
+  if Rails.env.development?
+      mount LetterOpenerWeb::Engine, at: "/letter_opener"
+    end
+end
